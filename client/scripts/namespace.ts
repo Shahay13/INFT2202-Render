d8c349f4d03d02a9afb617a68da617a68224b7fb
Shahay13 <takirul.islam@dcmail.ca>
2024-04-15 18:58:08 -0400

    First Commit

--- client/scripts/namespace.ts
+++ client/scripts/namespace.ts
@@ -1,15 +1,5 @@
-/*
- * File: namespace.ts
- * Author: Takirul Takirul
- * Date: April 13, 2024
- * Description: This file contains the core namespace which will encapsulate all the functionality of the application.
- */
-
 "use strict";
 
-/**
- * The core namespace encapsulates all the functionality of the application.
- */
 namespace core {
 
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,21 +9,20 @@ const contact_1 = __importDefault(require("../models/contact"));
 const user_1 = __importDefault(require("../models/user"));
 const index_1 = require("../util/index");
 const passport_1 = __importDefault(require("passport"));
-const post_1 = __importDefault(require("../models/post"));
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Home', page: "home", displayName: (0, index_1.UserDisplayName)(req) });
 });
 router.get('/home', function (req, res, next) {
     res.render('index', { title: 'Home', page: "home", displayName: (0, index_1.UserDisplayName)(req) });
 });
-router.get('/team', function (req, res, next) {
-    res.render('index', { title: 'Team', page: "team", displayName: (0, index_1.UserDisplayName)(req) });
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About Us', page: "about", displayName: (0, index_1.UserDisplayName)(req) });
 });
-router.get('/blog', function (req, res, next) {
-    res.render('index', { title: 'Blog', page: "blog", displayName: (0, index_1.UserDisplayName)(req) });
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact Us', page: "contact", displayName: (0, index_1.UserDisplayName)(req) });
 });
-router.get('/portfolio', function (req, res, next) {
-    res.render('index', { title: 'Portfolio', page: "portfolio", displayName: (0, index_1.UserDisplayName)(req) });
+router.get('/products', function (req, res, next) {
+    res.render('index', { title: 'Products', page: "products", displayName: (0, index_1.UserDisplayName)(req) });
 });
 router.get('/services', function (req, res, next) {
     res.render('index', { title: 'Services', page: "services", displayName: (0, index_1.UserDisplayName)(req) });
@@ -32,7 +31,7 @@ router.get('/login', function (req, res, next) {
     if (!req.user) {
         return res.render('index', { title: 'Login', page: "login", messages: req.flash('loginMessage'), displayName: (0, index_1.UserDisplayName)(req) });
     }
-    return res.redirect('/community-posts');
+    return res.redirect('/contact-list');
 });
 router.post('/login', function (req, res, next) {
     passport_1.default.authenticate('local', function (err, user, info) {
@@ -49,7 +48,7 @@ router.post('/login', function (req, res, next) {
                 console.error(err);
                 res.end();
             }
-            res.redirect('/community-posts');
+            res.redirect('/contact-list');
         });
     })(req, res, next);
 });
@@ -57,7 +56,7 @@ router.get('/register', function (req, res, next) {
     if (!req.user) {
         res.render('index', { title: 'Register', page: "register", messages: req.flash('registerMessage'), displayName: (0, index_1.UserDisplayName)(req) });
     }
-    return res.redirect('/community-posts');
+    return res.redirect('/contact-list');
 });
 router.post('/register', function (req, res, next) {
     let newUser = new user_1.default({
@@ -80,7 +79,7 @@ router.post('/register', function (req, res, next) {
         }
         else {
             return passport_1.default.authenticate('local')(req, res, function () {
-                return res.redirect('/community-posts');
+                return res.redirect('/contact-list');
             });
         }
     });
@@ -146,63 +145,6 @@ router.post('/add', index_1.AuthGuard, function (req, res, next) {
     });
     contact_1.default.create(newContact).then(function () {
         res.redirect('/contact-list');
-    }).catch(function (err) {
-        console.error(err);
-        res.end(err);
-    });
-});
-router.get('/community-posts', index_1.AuthGuard, function (req, res, next) {
-    post_1.default.find().then(function (data) {
-        res.render('index', { title: 'Community Posts', page: "community-posts", posts: data, displayName: (0, index_1.UserDisplayName)(req) });
-    }).catch(function (err) {
-        console.error("Encountered an Error reading posts from the Database: " + err);
-        res.end();
-    });
-});
-router.get('/edit-post/:id', index_1.AuthGuard, function (req, res, next) {
-    let id = req.params.id;
-    post_1.default.findById(id).then(function (postToEdit) {
-        res.render('index', { title: 'Edit Post', page: "edit-post", post: postToEdit, displayName: (0, index_1.UserDisplayName)(req) });
-    }).catch(function (err) {
-        console.error(err);
-        res.end();
-    });
-});
-router.get('/add-post', index_1.AuthGuard, function (req, res, next) {
-    res.render('index', { title: 'Add Post', page: "edit-post", post: '', displayName: (0, index_1.UserDisplayName)(req) });
-});
-router.get('/delete-post/:id', index_1.AuthGuard, function (req, res, next) {
-    let id = req.params.id;
-    post_1.default.deleteOne({ _id: id }).then(function () {
-        res.redirect('/community-posts');
-    }).catch(function (err) {
-        console.error(err);
-        res.end(err);
-    });
-});
-router.post('/edit-post/:id', index_1.AuthGuard, function (req, res, next) {
-    let id = req.params.id;
-    let updatedPost = new post_1.default({
-        "_id": id,
-        "PostTitle": req.body.postTitle,
-        "PostUser": req.body.postUser,
-        "PostComment": req.body.postComment
-    });
-    post_1.default.updateOne({ _id: id }, updatedPost).then(function () {
-        res.redirect('/community-posts');
-    }).catch(function (err) {
-        console.error(err);
-        res.end(err);
-    });
-});
-router.post('/add-post', index_1.AuthGuard, function (req, res, next) {
-    let newPost = new post_1.default({
-        "PostTitle": req.body.postTitle,
-        "PostUser": req.body.postUser,
-        "PostComment": req.body.postComment
-    });
-    post_1.default.create(newPost).then(function () {
-        res.redirect('/community-posts');
     }).catch(function (err) {
         console.error(err);
         res.end(err);
